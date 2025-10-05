5b6552395a58d5b7539aa3b1e2d712329f4f4bbd
BurnettMiao <searchfor1987@gmail.com>
2025-10-05 17:24:30 +0800

    navbar and heroSection初步整理ＯＫ

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -2,7 +2,11 @@
 
 <template>
   <div class="w-full h-[90vh]">
-    <img class="object-cover object-center" src="" alt="" />
+    <img
+      class="w-full h-full object-cover object-center"
+      src="https://images.unsplash.com/photo-1593335663758-4da70281a917?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      alt=""
+    />
   </div>
 </template>
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,14 @@ const navItems = ref(['關於我們', '我們目標', '活動紀錄', '商店', 
 </script>
 
 <template>
-  <nav class="flex items-center justify-between">
-    <span v-for="(item, index) in navItems" :key="index">{{ item }}</span>
+  <nav class="w-full flex items-center justify-center gap-12 py-[20px] fixed">
+    <div class="bg-white/25 flex py-[10px] px-[20px] rounded-4xl gap-12">
+      <span
+        v-for="(item, index) in navItems"
+        :key="index"
+        class="text-white opacity-100 cursor-pointer font-bold hover:text-moisten-bg-orange"
+        >{{ item }}</span
+      >
+    </div>
   </nav>
 </template>
