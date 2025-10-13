a7ddb7d8e4a0e76f8784efd679c11620473ead8f
BurnettMiao <searchfor1987@gmail.com>
2025-10-14 00:03:50 +0800

    調整SmallMenu.vue 文字

--- src/components/SmallMenu.vue
+++ src/components/SmallMenu.vue
@@ -11,7 +11,7 @@ const props = defineProps({
     class="fixed block md:hidden bg-moisten-white px-4 top-0 left-0 w-full h-screen pt-[90px] z-40"
   >
     <div v-for="(item, itemIndex) in props.navItems" :key="itemIndex">
-      <div class="py-3 text-lg font-bold text-center cursor-pointer hover:text-moisten-orange">
+      <div class="py-3 text-xl font-bold text-center cursor-pointer hover:text-moisten-orange">
         {{ item }}
       </div>
       <div
