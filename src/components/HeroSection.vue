47f16230191a85463c485f3e638728d5b51630ec
BurnettMiao <searchfor1987@gmail.com>
2025-10-14 11:16:59 +0800

    程式碼調整

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -6,6 +6,10 @@ const imgAndTextArr = ref([
     img: 'https://images.unsplash.com/photo-1593335663758-4da70281a917?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     text: 'Moisten',
   },
+  {
+    img: 'https://images.pexels.com/photos/1240961/pexels-photo-1240961.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    text: 'Moisten',
+  },
 ])
 
 // Import Swiper Vue.js components
